4ce8190f0fda78082dee2a1d3a921176694723ba
mjombanorman <mjomban@gmail.com>
2023-09-12 17:14:47 +0300

    Improved the admin model for the store section 	a. Added URL to show products under collection in the CollectionAdmin Section 	b. Added URL to show orders related to each customer in the CustomerAdmin seciton

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,34 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
 from .models import *
-# Register your models here.
-admin.site.register(Collection)
+from django.db.models import Count
+from django.utils.html import format_html,urlencode
+from django.urls import reverse
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price','inventory_status','collection_title']
+# Register your models here.
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title','products_count']
+
+    @admin.display(ordering='products_count')
+    def products_count(self,collection):
+        url = (
+            reverse('admin:store_product_changelist')
+              + '?'
+              + urlencode({
+                  'collection__id':str(collection.id)
+              }))
+        page_link = format_html ('<a href="{}">{}</a>',url,collection.products_count)
+        return page_link
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            products_count = Count('product')
+        )
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -24,11 +50,25 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'membership']
+    list_display = ['first_name', 'last_name','membership', 'orders']
     list_editable = ['membership']
-    ordering = ['first_name', 'last_name']
-    list_per_page = 20
+    list_per_page = 10
+  
+    @admin.display(ordering='orders')
+    def orders(self,customer):
+        url = (
+            reverse('admin:store_order_changelist')
+              + '?'
+              + urlencode({
+                  'customer__id':str(customer.id)
+              }))
+        page_link = format_html ('<a href="{}">{} Orders</a>',url,customer.orders_count)
+        return page_link
 
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            orders_count=Count('order')
+        )
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
