e88273c0953bf66ea17725dac460b7c6b9c52b44
mjombanorman <mjomban@gmail.com>
2023-09-12 15:51:36 +0300

    Registering of Customer model and adding of custom columns in products and customers

--- store/admin.py
+++ store/admin.py
@@ -2,4 +2,18 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(Collection)
-admin.site.register(Product)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price']
+    list_editable = ['unit_price']
+    list_per_page = 20
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    ordering = ['first_name', 'last_name']
+    list_per_page = 20
