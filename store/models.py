9e867d0ffde9552557f934efdc5e2e72be9e44b2
mjombanorman <mjomban@gmail.com>
2023-09-12 15:40:56 +0300

    Registering of key models

--- store/models.py
+++ store/models.py
@@ -10,6 +10,12 @@ class Collection(models.Model):
     title = models.CharField(max_length=100)
     featured_product = models.ForeignKey('Product',on_delete=models.SET_NULL,null=True,related_name='+')
 
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
+
 class Product(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
@@ -19,6 +25,12 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection,on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion,blank=True)
+
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
 
 
 class Customer(models.Model):
--- tag/models.py
+++ tag/models.py
@@ -17,7 +17,7 @@ class Tag(models.Model):
     label = models.CharField(max_length=100)
 
 class TaggedItem(models.Model):
-    objects =  TaggedItem()
+    objects =  TaggedItemManager()
     tag = models.ForeignKey(Tag,on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
