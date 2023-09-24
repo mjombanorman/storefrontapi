bbd1e6b1fa8925abe588b2e902ecb5b7023e931c
mjombanorman <mjomban@gmail.com>
2023-09-24 13:27:14 +0300

    Restructured the filer code to make it more simple and less bulky

--- store/views.py
+++ store/views.py
@@ -1,22 +1,22 @@
+from django.db.models import Count
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import *
 from .serializers import *
-from django.db.models import Count
+from .filters import *
+
+
+
 # Create your views here.
 
 class ProductViewSet(ModelViewSet):
-    
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
-    def get_queryset(self):
-        queryset = Product.objects.all()
-        collection_id = self.request.query_params.get('collection_id')
-        if collection_id is not None:
-            queryset = queryset.filter(collection_id=collection_id)
-        return queryset
-       
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ProductFilter
+    # filterset_fields = ['collection_id','unit_price']
 
     def get_serializer_context(self):
         return {'request': self.request}
