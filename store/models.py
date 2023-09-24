1dade7e54b986c9dfe1955de2103cc0030fa13f3
mjombanorman <mjomban@gmail.com>
2023-09-24 14:45:59 +0300

    Added migration for the Cart - Primary Key set to uuid,added related name to CartItems,created unique key constraint for one product per cart

--- store/models.py
+++ store/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-
 from django.core.validators import MinValueValidator
+from uuid import uuid4
+
+
 class Promotion(models.Model):
     description = models.CharField(max_length=255)
     discount = models.FloatField()
@@ -82,11 +84,15 @@ class Address(models.Model):
     customer = models.OneToOneField(Customer,on_delete=models.CASCADE,primary_key=True)
 
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart,on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart,on_delete=models.CASCADE,related_name='items')
     product = models.ForeignKey(Product,on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()  
+
+    class Meta:
+        unique_together = [['cart','product']]
 
 class Review(models.Model):
     product = models.ForeignKey(Product,on_delete=models.CASCADE,related_name='reviews')
