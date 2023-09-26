5ac77826c26d24bd9509b0dcff26b95300673754
mjombanorman <mjomban@gmail.com>
2023-09-26 21:59:36 +0300

    Allow retrieval of current user profile

--- core/serializers.py
+++ core/serializers.py
@@ -1,4 +1,5 @@
 from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+from djoser.serializers import UserSerializer as BaseUserSerializer
 from rest_framework import serializers
 
 
@@ -6,3 +7,8 @@ class UserCreateSerializer(BaseUserCreateSerializer):
     birth_date = serializers.DateField(required=False)
     class Meta(BaseUserCreateSerializer.Meta):
         fields = ['id','username','first_name','last_name','email','password']
+
+
+class UserSerializer(BaseUserSerializer):
+    class Meta(BaseUserSerializer.Meta):
+        fields = ['id','username','first_name','last_name','email']
