ac9e72d898cc3e13660c24a56a149154c7bb4b44
Mjomba Norman <mjomban@gmail.com>
2023-10-03 20:09:42 +0300

    Added OrderItems Serializers and allowed display of products in the OrderViewSet

--- store/serializers.py
+++ store/serializers.py
@@ -99,8 +99,18 @@ class CustomerSerializer(serializers.ModelSerializer):
         model = Customer
         fields = ['id','user_id','phone','birthdate','membership']
         
+       
+       
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = SimpleProductSerializer()
+    class Meta:
+        model = OrderItem
+        fields = ['id','product','quantity','unit_price']
         
 class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True)
     class Meta:
         model= Order
-        fields = ['id','customer']
+        fields = ['id','customer','placed_at','payment_status','items']
+        
+        
