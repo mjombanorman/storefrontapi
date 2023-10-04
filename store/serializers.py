f332ce3b30f658cece84636b543b5e8de3adec5b
Mjomba Norman <mjomban@gmail.com>
2023-10-04 19:15:16 +0300

    Allow creating of order items,saving and deleting the the cart after placing the order

--- store/serializers.py
+++ store/serializers.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 from rest_framework import serializers
 from store.models import *
+from django.db import transaction
     
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
@@ -117,9 +118,23 @@ class OrderSerializer(serializers.ModelSerializer):
         
 class CreateOrderSerializer(serializers.Serializer):
     cart_id = serializers.UUIDField()
+    
     def save(self,**kwargs):
-        print (self.validated_data['cart_id'])
-        print(self.context['user_id'])
-        
-        (customer,created)=Customer.objects.get_or_create(user_id=self.context['user_id'])
-        Order.objects.create(customer=customer)
+        with transaction.atomic():
+            # print (self.validated_data['cart_id'])
+            # print(self.context['user_id'])
+            cart_id = self.validated_data['cart_id']
+            (customer,created)=Customer.objects.get_or_create(user_id=self.context['user_id'])
+            order = Order.objects.create(customer=customer)
+            
+            cart_items =CartItem.objects.select_related('product').filter(cart_id=cart_id)
+            order_items= [
+                OrderItem(
+                order=order,
+                product=item.product,
+                unit_price=item.product.unit_price,
+                quantity = item.quantity
+            ) for item in cart_items]
+            
+            OrderItem.objects.bulk_create(order_items)
+            Cart.objects.filter(pk=cart_id).delete()
--- store/views.py
+++ store/views.py
@@ -111,20 +111,15 @@ class OrderViewSet(ModelViewSet):
     def get_serializer_class(self):
         if self.request.method == 'POST':
             return CreateOrderSerializer
-        
         return OrderSerializer
-        
     
     def get_serializer_context(self):
-        
         return {'user_id':self.request.user.id}
-    
-    
+        
     def get_queryset(self):
         user = self.request.user
         if user.is_staff:
-            return Order.objects.all()
-        
+            return Order.objects.all() 
         user_id= Customer.objects.only('id').get(user_id=user.id)
         Order.objects.filter(customer_id=user_id)
    
