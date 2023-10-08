47d1da6407dac337af964df8c0099a47d94a1672
mjombanorman <mjomban@gmail.com>
2023-10-08 18:29:36 +0300

    complete celery config

--- storefront/celery.py
+++ storefront/celery.py
@@ -1,6 +1,6 @@
 import os
 from celery import Celery
-os.environ.setdefault('DJANGO_SETTINGS.MODULE','storefront.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','storefront.settings')
 celery = Celery('storefront')
 celery.config_from_object('django.conf:settings', namespace='CELERY')
 celery.autodiscover_tasks()
