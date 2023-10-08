df90965bed2512939e3aa98feb9d9757d5f66aca
mjombanorman <mjomban@gmail.com>
2023-10-08 15:37:40 +0300

    Image validation of product image

--- store/models.py
+++ store/models.py
@@ -3,6 +3,7 @@ from django.core.validators import MinValueValidator
 from uuid import uuid4
 from django.conf import settings
 from django.contrib import admin
+from .validators import validate_file_size
 
 
 class Promotion(models.Model):
@@ -38,7 +39,7 @@ class Product(models.Model):
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product,on_delete=models.CASCADE,related_name='images')
-    image = models.ImageField(upload_to='store/images')
+    image = models.ImageField(upload_to='store/images',validators=[validate_file_size])
       
     def __str__(self) -> str:
         return self.product.title
