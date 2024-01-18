fbc4865e96732db4bad708adb348eedf294699c3
mjombanorman <mjomban@gmail.com>
2024-01-18 22:12:09 +0300

    Setting up add to cart functionality to add via URL

--- frontend/src/pageassets/Product.jsx
+++ frontend/src/pageassets/Product.jsx
@@ -1,3 +1,35 @@
+// import React, { Component } from "react";
+
+// class Product extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.handleAddToCart = this.handleAddToCart.bind(this);
+//   }
+//   handleAddToCart(e) {
+//     this.props.handleItemClick(); //addToCart(this.props.item);
+//   }
+//   render() {
+//     return (
+//       <div className="items">
+//         <img src={this.props.item.image}></img>
+//         <div className="info">
+//           <h3>{this.props.item.name}</h3>
+//           <span>£ {this.props.item.price}</span>
+//           <button
+//             onClick={this.handleAddToCart}
+//             disabled={this.props.item.inCart}
+//             className={this.props.item.inCart ? "button-disabled" : ""}>
+//             {this.props.item.inCart ? "Item in a cart" : "Add to cart"}
+//           </button>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Product;
+
+// Product.js
 import React, { Component } from "react";
 
 class Product extends Component {
@@ -5,21 +37,27 @@ class Product extends Component {
     super(props);
     this.handleAddToCart = this.handleAddToCart.bind(this);
   }
-  handleAddToCart(e) {
-    this.props.addToCart(this.props.item);
+
+  handleAddToCart() {
+    this.props.addToCart(this.props.item); // Call the addToCart prop with the item
   }
+
   render() {
+    const { item } = this.props;
+
     return (
       <div className="items">
-        <img src={this.props.item.image}></img>
+        {/* <img src={item.image} alt={item.name}></img> */}
         <div className="info">
-          <h3>{this.props.item.name}</h3>
-          <span>£ {this.props.item.price}</span>
+          <h3>{item.title}</h3>
+          <p>{item.description}</p>
+          <span>$ {item.unit_price}</span>
+
           <button
             onClick={this.handleAddToCart}
-            disabled={this.props.item.inCart}
-            className={this.props.item.inCart ? "button-disabled" : ""}>
-            {this.props.item.inCart ? "Item in a cart" : "Add to cart"}
+            disabled={item.inCart}
+            className={item.inCart ? "button-disabled" : ""}>
+            {item.inCart ? "Item in a cart" : "Add to cart"}
           </button>
         </div>
       </div>
--- frontend/src/pageassets/ProductList.jsx
+++ frontend/src/pageassets/ProductList.jsx
@@ -1,13 +1,141 @@
+// // // import React, { Component } from "react";
+// // // import Product from "./Product";
+
+// // // class ProductList extends Component {
+// // //   render() {
+// // //     let items = this.props.items.map((item, index) => {
+// // //       return (
+// // //         <Product key={item.id} item={item} addToCart={this.props.handleClickItem} />
+// // //       );
+// // //     });
+// // //     return <section id="list">{items}</section>;
+// // //   }
+// // // }
+
+// // // export default ProductList;
+// // import React, { Component } from "react";
+// // import Product from "./Product";
+
+// // class ProductList extends Component {
+// //   render() {
+// //     let items = this.props.items.map((item, index) => {
+// //       return (
+// //         <Product
+// //           key={item.id}
+// //           item={item}
+// //           handleItemClick={this.props.handleItemClick}
+// //         />
+
+// //         // <Product
+// //         //   key={item.id}
+// //         //   item={item}
+// //         //   handleAddToCart={this.props.handleItemClick}
+// //         // />
+// //       );
+// //     });
+// //     return <section id="list">{items}</section>;
+// //   }
+// // }
+
+// // export default ProductList;
+// // ProductList.js
+// import React, { Component } from "react";
+// import Product from "./Product";
+
+// class ProductList extends Component {
+//   constructor(props) {
+//     super(props);
+//   //  this.handleRemoveFromCart = this.handleRemoveFromCart.bind(this);
+//     this.fetchProducts = this.fetchProducts.bind(this);
+//       this.state = {
+//         items:[],
+     
+//       };
+//   }
+
+
+//   async fetchProducts() {
+//       try {
+//         const response = await api.get("store/products/");
+
+//         if (response.ok) {
+//           // Handle success
+//           this.setState({ items: response.data });
+//           console.log(response.data);
+//           console.log("POST request successful");
+//         } else {
+//           // Handle error
+//           console.error("POST request failed");
+//         }
+//       } catch (error) {
+//         console.error("Error:", error);
+//       }
+//     }
+  
+//   render() {
+//     let items = this.items.map((item, index) => {
+//       return (
+//         <Product
+//           key={item.id}
+//           item={item.title}
+//           addToCart={this.props.addToCart} // Use addToCart instead of handleItemClick
+//         />
+//       );
+//     });
+//     return <section id="list">{items}</section>;
+//   }
+// }
+
+// export default ProductList;
 import React, { Component } from "react";
 import Product from "./Product";
+import api from "../helpers/Gateway"; // Import your API helper
 
 class ProductList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+    };
+  }
+
+  // Call fetchProducts when the component mounts
+  componentDidMount() {
+    this.fetchProducts();
+  }
+
+  async fetchProducts() {
+    try {
+      const response = await api.get("store/products");
+
+      if (response.status !== 200) {
+        throw new Error(`Request failed with status: ${response.status}`);
+      }
+
+      const data = await response.data.results;
+      this.setState({ items: data });
+      console.log("data",data);
+      console.log("GET request successful");
+    } catch (error) {
+      console.error("Error:", error.message);
+      // You can add additional error handling logic here, e.g., displaying an error message to the user.
+    }
+  }
+
+  
   render() {
-    let items = this.props.items.map((item, index) => {
+    //send item as props to product page
+    console.log("items",this.state.items);
+    let items = this.state.items.map((item) => {
       return (
-        <Product key={item.id} item={item} addToCart={this.props.addToCart} />
+        <Product
+          key={item.id}
+          item={item} // Pass the whole item object to Product
+          addToCart={this.props.addToCart}
+        />
       );
     });
+
     return <section id="list">{items}</section>;
   }
 }
